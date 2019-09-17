ce54e4a36650d208cfeee311b6cc5102de19e2fe
michaellee212 <33797607+michaellee212@users.noreply.github.com>
2019-09-17 12:53:18 -0700

    v4.2- styled components
    added styled components to custom button and header/homepage styles.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,3 +1,13 @@
+// import React from 'react';
+
+// import { CustomButtonContainer } from './custom-button.styles';
+
+// const CustomButton = ({ children, ...props }) => (
+// 	<CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+// );
+
+// export default CustomButton;
+
 import React from 'react';
 
 import './custom-button.styles.scss';
