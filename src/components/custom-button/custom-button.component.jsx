3ef683d46ce10abb33c52673ec1d9144112277f5
michaellee212 <33797607+michaellee212@users.noreply.github.com>
2019-09-19 15:18:41 -0700

    v4.6-async redux
    fixed bugs

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-
-import { CustomButtonContainer } from './custom-button.styles';
-
-const CustomButton = ({ children, ...props }) => (
-	<CustomButtonContainer {...props}>{children}</CustomButtonContainer>
-);
-
-export default CustomButton;
-
 // import React from 'react';
 
-// import './custom-button.styles.scss';
+// import { CustomButtonContainer } from './custom-button.styles';
 
-// const CustomButton = ({
-// 	children,
-// 	inverted,
-// 	isGoogleSignIn,
-// 	...otherProps
-// }) => (
-// 	<button
-// 		className={`${inverted ? 'inverted' : ''} ${
-// 			isGoogleSignIn ? 'google-sign-in' : ''
-// 		} custom-button`}
-// 		{...otherProps}
-// 	>
-// 		{children}
-// 	</button>
+// const CustomButton = ({ children, ...props }) => (
+// 	<CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 // );
 
 // export default CustomButton;
+
+import React from 'react';
+
+import './custom-button.styles.scss';
+
+const CustomButton = ({
+	children,
+	inverted,
+	isGoogleSignIn,
+	...otherProps
+}) => (
+	<button
+		className={`${inverted ? 'inverted' : ''} ${
+			isGoogleSignIn ? 'google-sign-in' : ''
+		} custom-button`}
+		{...otherProps}
+	>
+		{children}
+	</button>
+);
+
+export default CustomButton;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,7 +7,10 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component';
 
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+import {
+	selectIsCollectionFetching,
+	selectIsCollectionsLoaded
+} from '../../redux/shop/shop.selectors';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
@@ -21,7 +24,7 @@ class ShopPage extends React.Component {
 	}
 
 	render() {
-		const { match, isCollectionFetching } = this.props;
+		const { match, isCollectionFetching, isCollectionLoaded } = this.props;
 		return (
 			<div className='shop-page'>
 				<Route
@@ -38,7 +41,7 @@ class ShopPage extends React.Component {
 					path={`${match.path}/:collectionId`}
 					render={props => (
 						<CollectionsPageWithSpinner
-							isLoading={isCollectionFetching}
+							isLoading={!isCollectionLoaded}
 							{...props}
 						/>
 					)}
@@ -49,7 +52,8 @@ class ShopPage extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-	isCollectionFetching: selectIsCollectionFetching
+	isCollectionFetching: selectIsCollectionFetching,
+	isCollectionLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
