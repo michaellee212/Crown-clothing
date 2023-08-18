0dd46d7d0a603ec17b91b88c34bcf9e8c4e4536c
Michael Lee <michaelee212@gmail.com>
2023-08-17 17:32:51 -0700

    fixed npm run build

---
+++ client/build/precache-manifest.22675d85c35f3a97bcd0b9958a7d5bfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e161a3b41723452aa08c6296c7d84f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "427491143336a4874d33",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6b724e2cb4d4e2c09373",
+    "url": "/static/js/2.898beb1d.chunk.js"
+  },
+  {
+    "revision": "427491143336a4874d33",
+    "url": "/static/js/main.20ed8a51.chunk.js"
+  },
+  {
+    "revision": "48d41c13aacf878df560",
+    "url": "/static/js/runtime~main.b0422439.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
